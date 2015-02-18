f275527565f64b3296d45b864532748b1df14b03
Adam Napora <anapora@apple.com>
2015-02-18 19:59:56 +0000

    Can log in and retrieve user

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -17,4 +17,44 @@ class LoginController extends AbstractController
     {
         $this->loadView('login/index');
     }
+
+    /**
+     * Validate username and password and redirect to home on success
+     */
+    public function validateAction()
+    {
+        // retrieve username
+        $username = $_POST['username'];
+
+        // validate username
+        // @todo
+
+        // retrieve password
+        $password = $_POST['password'];
+
+        // validate password
+        // @todo
+
+
+        try {
+            // check with db
+            $userModel = new UserModel($username, $password);
+
+            $foundUser = $userModel->findUser($userModel);
+
+            echo 'result:';
+            print_r($foundUser);
+
+        } catch (\Exception $e) {
+
+            // on any exception - apply global error handler,
+            // and display default error page
+            $this->handleError($e);
+        }
+
+
+        // redirect to home on success or back to login/index
+        // on failure
+
+    }
 }
