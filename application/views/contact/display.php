1149688ff994199b82814f6ae59d3a9a487fa4b2
gregoryjokiel <grzegorz.jokiel@mycit.ie>
2015-02-19 23:18:33 +0000

    display.php pulled

---
+++ application/views/contact/display.php
@@ -0,0 +1,37 @@
+
+<div class="well">
+    <h4>Display Contacts</h4>
+</div>
+
+<?php if (!count($contacts)): ?>
+
+    <div class="alert alert-warning">
+        <h2>No contacts found.</h2>
+    </div>
+
+<?php else: ?>
+
+    <table class="table table-bordered table-responsive table-striped">
+        <thead>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Date Of Birth</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <?php foreach ($contacts as $contact): ?>
+
+            <tr>
+                <td><?php echo $contact['firstName'] ?></td>
+                <td><?php echo $contact['lastName'] ?></td>
+                <td><?php echo $contact['birthday'] ?></td>
+            </tr>
+
+        <?php endforeach ?>
+
+        </tbody>
+    </table>
+
+<?php endif ?>
