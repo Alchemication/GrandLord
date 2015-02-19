d30313049075cba628748ce8d474de29e8c1d6d1
gregoryjokiel <grzegorz.jokiel@mycit.ie>
2015-02-19 22:48:58 +0000

    Database tables created

--- application/sql/db.sql
+++ application/sql/db.sql
@@ -1,4 +1,46 @@
+/**
+ * Created by: Grzegorz Jokiel
+ * Date: 19/02/15
+ * Time: 22:20
+ */
 create database grandlord;
+/**
+
+ FOREIGN KEY (roleId) REFERENCES user_roles(id), - in user table
+*/
+
+CREATE TABLE user_roles
+( id INT(11) NOT NULL AUTO_INCREMENT,
+  CONSTRAINT user_roles_pk PRIMARY KEY (id),
+  roleId INT (11),
+  name ENUM('tenant', 'landlord', 'admin')
+);
+
+insert into user_roles (id, roleId, name) values (null, '1', 'tenant');
+insert into user_roles (id, roleId, name) values (null, '2', 'landlord');
+insert into user_roles (id, roleId, name) values (null, '3', 'admin');
+
+
+CREATE TABLE user
+( id INT(11) NOT NULL AUTO_INCREMENT,
+  CONSTRAINT user_pk PRIMARY KEY (id),
+  roleId INT (11),
+  FOREIGN KEY (roleId) REFERENCES user_roles(id),
+  username VARCHAR(255),
+  password VARCHAR(512),
+  email   VARCHAR(255),
+  firstName VARCHAR (255),
+  secondName VARCHAR (255),
+  createdAt DATETIME,
+  updatedAt DATETIME,
+  active ENUM('y', 'n')
+);
+
+insert into user (id, roleId, username, password, email, firstName, secondName, createdAt, updatedAt, active ) values (null, '1', 'gjokiel', 'gjokiel123', 'gjok@mycit.ie','Greg', 'Jokiel', '2015-02-19 23:59:59', '2015-02-19 23:59:59', 'y');
+insert into user (id, roleId, username, password, email, firstName, secondName, createdAt, updatedAt, active ) values (null, '1', 'anapora', 'anapora123', 'anap@mycit.ie','Adam', 'Napora', '2015-02-19 23:59:59', '2015-02-19 23:59:59', 'y');
+insert into user (id, roleId, username, password, email, firstName, secondName, createdAt, updatedAt, active ) values (null, '1', 'pbaran', 'pbaranl123', 'pbar@mycit.ie','Piotr', 'Baran', '2015-02-19 23:59:59', '2015-02-19 23:59:59', 'y');
+insert into user (id, roleId, username, password, email, firstName, secondName, createdAt, updatedAt, active ) values (null, '3', 'admin', 'adminl123', 'admin@mycit.ie','Admin', null, '2015-02-19 23:59:59', '2015-02-19 23:59:59', 'y');
+
 
 CREATE TABLE contacts
 ( id INT(11) NOT NULL AUTO_INCREMENT,
@@ -12,11 +54,3 @@ insert into contacts (id, lastName, firstName, birthday) values (null, 'Napora',
 insert into contacts (id, lastName, firstName, birthday) values (null, 'Jokiel', 'Greg', '1973-01-11 10:00:10');
 insert into contacts (id, lastName, firstName, birthday) values (null, 'Baran', 'Piotr', '1963-03-28 09:45:12');
 
-CREATE TABLE users
-( id INT(11) NOT NULL AUTO_INCREMENT,
-  username VARCHAR(255),
-  password VARCHAR(512),
-  CONSTRAINT users_pk PRIMARY KEY (id)
-);
-
-insert into users values (null, 'greg', 'greg123');
