7fb674c1a78c35f9dbc91471baed6bfdf02e1cf7
gregoryjokiel <grzegorz.jokiel@mycit.ie>
2015-02-24 23:16:49 +0000

    Home page updated

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,32 @@
 <!-- Jumbotron -->
 <div class="jumbotron">
-    <h1>Home Website!</h1>
-    <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-    <p> Testing header and footer </p>
+    <h1>Project Grandlord</h1>
+    <p class="lead">Software Project</p>
+    <p class="lead">CIT 2015</p>
+    <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+    <p> Project Drivers
+        1.  The Purpose of the Project.
+        2.  The Stakeholders
+
+        Project Constraints
+        3.  Mandated Constraints
+        4.  Naming Conventions and Definit
+        5.  Relevant Facts and Assumptions
+
+        Functional Requirements
+        6.  The Scope of the Work
+        7.  Business Data Model
+        8.  The Scope of the Product
+        9.  Functional Requirements
+
+        Non-functional Requirements
+        10. Look and Feel Requirements
+        11. Usability Requirements
+        12. Performance Requirements
+        13. Operational and Environmental R
+        14. Security Requirements
+        15. Legal Requirements
+
+        Appendix
+        1.  Interview questions </p>
 </div>
