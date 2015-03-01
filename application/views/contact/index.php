9a2c6eec59ea3b15e5c930c59fcdd74905590377
gregoryjokiel <grzegorz.jokiel@mycit.ie>
2015-03-01 17:00:10 +0000

    Contact form created.

--- application/views/contact/index.php
+++ application/views/contact/index.php
@@ -1,7 +1,48 @@
 
 <div class="jumbotron">
+
+    <div class="form-group">
+        <h2>Contact Us</h2>
+    </div>
+
+    <form class="form-horizontal">
+        <div class="form-group">
+            <label for="inputName3" class="col-sm-2 control-label">Your Name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputName3" placeholder="Name">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputEmail3" class="col-sm-2 control-label">Your Email</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputPhone3" class="col-sm-2 control-label">Your Phone</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="inputPhone3" placeholder="Phone Number">
+            </div>
+        </div>
+
+        <div  class="form-group">
+            <label for="inputQuery3" class="col-sm-2 control-label">Your Query</label>
+            <div class="col-sm-10">
+            <textarea class="form-control" rows="3"></textarea>
+                </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+        </div>
+    </form>
+
 <div class="well">
-    <h4>Display Contacts</h4>
+    <h4>Staff Members</h4>
 </div>
 
 <?php if (!count($contacts)): ?>
@@ -36,4 +77,7 @@
     </table>
 
 <?php endif ?>
+
+
+
  </div>
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,32 +1,32 @@
 <!-- Jumbotron -->
-<div class="jumbotron">
+<div class="jumbotron" xmlns="http://www.w3.org/1999/html">
     <h1>Project Grandlord</h1>
-    <p class="lead">Software Project</p>
+    <p class="lead">Software Group Project - SOFT7009</p>
     <p class="lead">CIT 2015</p>
-    <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
-    <p> Project Drivers
-        1.  The Purpose of the Project.
-        2.  The Stakeholders
 
-        Project Constraints
-        3.  Mandated Constraints
-        4.  Naming Conventions and Definit
-        5.  Relevant Facts and Assumptions
+    <p><h4> Project Drivers</h4></br>
+        1.  The Purpose of the Project. </br>
+        2.  The Stakeholders</br>
 
-        Functional Requirements
-        6.  The Scope of the Work
-        7.  Business Data Model
-        8.  The Scope of the Product
-        9.  Functional Requirements
+        <h4>Project Constraints</h4></br>
+        3.  Mandated Constraints</br>
+        4.  Naming Conventions and Definit</br>
+        5.  Relevant Facts and Assumptions</br>
 
-        Non-functional Requirements
-        10. Look and Feel Requirements
-        11. Usability Requirements
-        12. Performance Requirements
-        13. Operational and Environmental R
-        14. Security Requirements
-        15. Legal Requirements
+        <h4>Functional Requirements</h4></br>
+        6.  The Scope of the Work</br>
+        7.  Business Data Model</br>
+        8.  The Scope of the Product</br>
+        9.  Functional Requirements</br>
 
-        Appendix
+        <h4>Non-functional Requirements</h4></br>
+        10. Look and Feel Requirements</br>
+        11. Usability Requirements</br>
+        12. Performance Requirements</br>
+        13. Operational and Environmental R</br>
+        14. Security Requirements</br>
+        15. Legal Requirements</br>
+
+        <h4>Appendix</h4></br>
         1.  Interview questions </p>
 </div>
