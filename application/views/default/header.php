30e594c654088632699ab55aabb00c14d4617b6f
Adam Napora <anapora@apple.com>
2015-03-08 10:23:13 +0000

    Added libraries and search text box to home page

--- application/views/default/header.php
+++ application/views/default/header.php
@@ -10,6 +10,10 @@
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>/public/css/bootstrap.css" />
 
+    <!-- JS Lib -->
+    <script src="<?php echo BASE_URL; ?>/public/js/jquery.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>/public/js/underscore.js"></script>
+
     <!-- Custom styles -->
     <link href="<?php echo BASE_URL ?>/public/css/justified-nav.css" rel="stylesheet">
     <link href="<?php echo BASE_URL ?>/public/css/stylesG.css" rel="stylesheet" type="text/css">
@@ -33,7 +37,7 @@
         <nav>
             <ul class="nav nav-justified">
                 <!-- class="active" to be defined -->
-                
+
                 <li class="<?php if ($currentView === 'home/index') { echo 'active'; } ?>"><a href="<?php echo BASE_URL ?>/home/index">Home</a></li>
                 <li class="<?php if ($currentView === 'search/index') { echo 'active'; } ?>"><a href="<?php echo BASE_URL ?>/search/index">Search</a></li>
                 <li class="<?php if ($currentView === 'myTenancies/index') { echo 'active'; } ?>"><a href="<?php echo BASE_URL ?>/myTenancies/index">My Tenancies</a></li>
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,9 +1,15 @@
 <!-- Jumbotron -->
 <div class="jumbotron" xmlns="http://www.w3.org/1999/html">
 
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>/public/css/search.css"/>
+
+    <input id="search-bar" type="text" placeholder="Enter address ..."/>
+
     <h1>Project GrandLord</h1>
     <p class="lead">Web App for rating Landlord's performance</p>
     <p class="lead">Software Group Project - SOFT7009</p>
     <p class="lead">CIT 2015</p>
 
 </div>
+
+<script src="<?php echo BASE_URL; ?>/public/js/search.js"></script>
