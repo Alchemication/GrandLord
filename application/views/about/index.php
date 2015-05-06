fecb44faa0abf319594df2d703eb44064b080585
gregoryjokiel <grzegorz.jokiel@mycit.ie>
2015-05-06 20:21:42 +0100

    Typos corrected.

--- application/views/about/index.php
+++ application/views/about/index.php
@@ -5,15 +5,15 @@
 
 <div  id="aboutContent">
 
-   <p>Grandlord website has been developed by three CIT students in 2014/2015 as part of a student project.</p>
-   <p>The idea to create this website has been inspired by real life experiences and most of all lack of information about landlords renting properties.</p>
+   <p>Grandlord website has been developed by three CIT students in 2014/2015 academic year as part of a student project.</p>
+   <p>The idea to create this website has been inspired by real life experiences and most of all lack of information about landlords who are renting properties.</p>
    <br>
-   <p>At the time of creating this project there is no website or system that enables tenants to check reviews on properties
-   landlords and agencies that offer accommodation to rent in Ireland</p>
+   <p>At the time of creating this project there is no website or system that enables tenants to check reviews on properties,
+   landlords and agencies that offer accommodation to rent in Ireland.</p>
    <br>
    <p>Many tenants would like to see other tenants opinions about landlord or agency advertising accommodation to rent.</p>
    <p>Lack of information about landlords and properties causes unnecessary risk and uncertainty before committing to live in certain place for the time specified in a lease contract.
-   In order to enable tenants to check reviews on properties there is a system required that will gather all required information for future reference of other users.
+   In order to enable tenants to check reviews on properties there is a system required that will gather all relevant information for future reference.
    We believe that GrandLord System will enable all interested parties to seek required information about properties and owners helping everyone to meet their expectations.</p>
 
    <h4>Project developers:</h4>
